5ad5b9c5912740ac64eab1607f963c0ec91e85b1
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-03-20 18:44:52 -0300

    GifExpertApp 20%

--- workspace/04-Gif-expert-app/src/GifExpertApp.js
+++ workspace/04-Gif-expert-app/src/GifExpertApp.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
-    const categories = ['One punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch']);
 
     return (
         <>
             <h2>Gif Expert App</h2>
+            <AddCategory setCategories={setCategories} />
             <hr />
 
             <ol>
                 {
-                    categories.map(category => {
-                        return <li>Hola</li>
-                    })
+                    categories.map(category => (
+                        <GifGrid
+                            key={category}
+                            category={category}
+                        />
+                    ))
                 }
             </ol>
         </>
     )
+}
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
 }
