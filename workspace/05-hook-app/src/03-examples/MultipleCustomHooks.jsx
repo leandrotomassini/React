4c68baa3a6d81895a3241f4eb8f87be57555037a
Leandro C. Tomassini <leandroctomassini@gmail.com>
2023-11-02 14:57:06 -0300

    UseCounter

--- workspace/05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ workspace/05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,10 +1,34 @@
-
+import { useFetch } from '../hooks/useFetch';
+import { useCounter } from '../hooks/useCounter';
 
 export const MultipleCustomHooks = () => {
+1.56
+
+  const { } = useCounter();
+  const { data, isLoading, hasError } = useFetch('https://rickandmortyapi.com/api/character/2');
+  const { name, species } = !!data && data;
+
   return (
     <>
-        <h1>Breaking Bad Quotes</h1>
-        <hr />
+      <h1>The Rick and Morty API</h1>
+      <hr />
+
+      {
+        (isLoading) ?
+          (
+            <div className='alert alert-info text-center'>
+              Loading...
+            </div>
+          )
+          : (
+            <blockquote className='blockquote'>
+              <h1 className='mb-1'>{name}</h1>
+              <h3 className='blockquote-footer mt-2'>{species}</h3>
+            </blockquote>
+          )
+      }
+
+      <button className='btn btn-primary'>Next quote</button>
     </>
-  )
+  );
 }
