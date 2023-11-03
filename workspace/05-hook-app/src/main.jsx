9eb2e49e4bc2c0e1aadfe99377831b525729d16e
Leandro C. Tomassini <leandroctomassini@gmail.com>
2023-11-03 15:19:18 -0300

    useReducer

--- workspace/05-hook-app/src/main.jsx
+++ workspace/05-hook-app/src/main.jsx
@@ -15,11 +15,12 @@ import ReactDOM from 'react-dom/client';
 // import { CallbackHook } from './06-memos/CallbackHook';
 import { Padre } from './07-tarea-memo/Padre';
 
+import './08-useReducer/intro-reducer';
 
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <Padre />
-  // </React.StrictMode>
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//   <Padre />
+//   // </React.StrictMode>
+// )
