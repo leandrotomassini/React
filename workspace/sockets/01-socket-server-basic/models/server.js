e7ead34f5fea86f3658a021c5dc8974bd706dc22
Leandro C. Tomassini <leandroctomassini@gmail.com>
2024-03-02 19:49:51 -0300

    cors

--- workspace/sockets/01-socket-server-basic/models/server.js
+++ workspace/sockets/01-socket-server-basic/models/server.js
@@ -3,6 +3,7 @@ const express = require( 'express' );
 const http = require( 'http' );
 const socketio = require( 'socket.io' );
 const path = require( 'path' );
+const cors = require( 'cors' );
 
 const Sockets = require( './sockets' );
 
@@ -25,6 +26,9 @@ class Server {
 
     // Desplegar el directorio público
     this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
+
+    // CORS
+    this.app.use( cors() );
 
   }
 
