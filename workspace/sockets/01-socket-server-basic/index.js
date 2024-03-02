65b88b4ea751914cd8cc590c058a6843c04ddc8f
Leandro C. Tomassini <leandroctomassini@gmail.com>
2024-03-02 19:39:34 -0300

    Url socket server

--- workspace/sockets/01-socket-server-basic/index.js
+++ workspace/sockets/01-socket-server-basic/index.js
@@ -1,29 +1,6 @@
-// Servidor de Express
-const express = require( 'express' );
-const app = express();
+const Server = require( './models/server' );
+require( 'dotenv' ).config();
 
-// Servidor de sockets
-const server = require( 'http' ).createServer( app );
+const server = new Server();
 
-// Configuración del socket server
-const io = require( 'socket.io' )( server );
-
-
-// Desplegar el directorio público
-app.use( express.static( __dirname + '/public' ) );
-
-io.on( 'connection', ( socket ) => {
-
-  socket.on( 'mensaje-to-server', ( data ) => {
-
-    console.log( { data } );
-
-    io.emit( 'mensaje-from-server', data );
-  } );
-
-} );
-
-
-server.listen( 8080, () => {
-  console.log( `Server corriendo en el puerto: 8080` );
-} );
+server.execute();
